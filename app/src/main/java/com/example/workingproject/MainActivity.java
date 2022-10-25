1faedf9c1340255e4e10bc2d97612157ebbe552f
jp2137 <sbaiyassine2004@gmail.com>
2022-10-25 11:04:25 +0200

    niby dziala ale da sie wybrac pare checkboxow trzea naprawic

--- app/src/main/java/com/example/workingproject/MainActivity.java
+++ app/src/main/java/com/example/workingproject/MainActivity.java
@@ -6,33 +6,36 @@ import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public String categories[]={"jabłko","pomarańcza","gruszka","mango","porzeczka"};
-    LinearLayout a = new LinearLayout(this.getApplicationContext());
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try{
+            String[] categories ={"jabłko","pomarańcza","gruszka","mango","porzeczka"};
+            LinearLayout a = findViewById(R.id.main_container);
+            for(int x = 0; x < 5; x++){
+                CheckBox checkBox1 = new CheckBox(getApplicationContext());
+                CheckBox checkBox2 = new CheckBox(getApplicationContext());
+                TextView textview1 = new TextView(getApplicationContext());
+                textview1.setText(categories[x]);
 
-        for(int x = 0; x < categories.length; x++){
-            CheckBox checkBox1 = new CheckBox(getApplicationContext());
-            CheckBox checkBox2 = new CheckBox(getApplicationContext());
-            TextView txtview1 = new TextView(getApplicationContext());
-            txtview1.setText(categories[x]);
+                LinearLayout b = new LinearLayout(getApplicationContext());
+                b.setOrientation(LinearLayout.HORIZONTAL);
 
-            LinearLayout b = new LinearLayout(getApplicationContext());
-            b.setOrientation(LinearLayout.VERTICAL);
+                b.addView(checkBox1);
+                b.addView(checkBox2);
+                b.addView(textview1);
 
-            b.addView(checkBox1);
-            b.addView(checkBox2);
-            b.addView(txtview1);
+                a.addView(b);
+            }
 
-            a.addView(b);
+        }catch (Error e){
+            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
         }
 
-        setContentView(R.layout.activity_main);
     }
 
 
